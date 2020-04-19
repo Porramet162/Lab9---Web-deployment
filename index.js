b2f27389f8c20b0fdf3b6b5ddec0c0300e1cfdaf
Porramet162 <63979071+Porramet162@users.noreply.github.com>
2020-04-20 03:53:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express')
+let app = express()
+
+app.get('/',(req, res) => {
+    console.log('Hello world')
+    res.send('Hello word')
+})
+
+app.get('/crash',(req, res) => {
+    console.log('Crash!!')
+    res.send('Crash')
+    process.exit(1)
+})
+app.listen(process.env.PORT, () => console.log('Sever is running at ', process.env.PORT))
+
